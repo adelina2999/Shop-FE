5c977e964e15b0c2cb5c04463025587411d67f0b
adelina2999 <tataru.adelina99@gmail.com>
2020-07-31 13:08:16 +0300

    Created Header component

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div v-for="laptop in laptops" 
+      :key="laptop.id">
+      <span> {{ laptop.name }} </span>
+      <span> {{ laptop.price }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      laptops: []
+    }
+  },
+  methods: {
+    getLaptops () {
+      axios.get('http://localhost:5050/laptops')
+        .then(response => {
+          this.laptops = response.data
+        })
+    }
+  },
+  created () {
+    this.getLaptops()
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import { BootstrapVue } from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   store,
