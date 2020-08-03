7fb2c1299fb6de0270f6e3196650e6b46a93a139
adelina2999 <tataru.adelina99@gmail.com>
2020-08-03 13:12:22 +0300

    modified the navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,82 +1,55 @@
 <template>
   <div>
 		<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
-  		<a class="navbar-brand" href="#">Navbar</a>
+  		<a class="navbar-brand">
+				<img class="logo-dimensions" src="../assets/logo.jpg">
+			</a>
   		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    	<span class="navbar-toggler-icon"></span>
-  </button>
+    		<span class="navbar-toggler-icon"></span>
+ 			</button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">
-          <i class="fa fa-home"></i>
-          Home
-          <span class="sr-only">(current)</span>
-          </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fa fa-envelope-o">
-            <span class="badge badge-danger">11</span>
-          </i>
-          Link
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">
-          <i class="fa fa-envelope-o">
-            <span class="badge badge-warning">new</span>
-          </i>
-          Disabled
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fa fa-envelope-o">
-            <span class="badge badge-primary">11</span>
-          </i>
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-    <ul class="navbar-nav ">
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fa fa-bell">
-            <span class="badge badge-info">11</span>
-          </i>
-          Test
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="fa fa-globe">
-            <span class="badge badge-success">11</span>
-          </i>
-          Test
-        </a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-    
+  		<div class="collapse navbar-collapse">
+    		<ul class="navbar-nav mr-auto">
+    	  	<li class="nav-item active">
+    	   	  <a class="nav-link" href="#">
+    	      	<i class="fa fa-laptop"></i>
+    	      	Laptops
+    	      </a>
+    	  	</li>
+    	  	<li class="nav-item active">
+    	    	<a class="nav-link" href="#">
+    	      	<i class="fa fa-phone"></i>
+							Phones
+    	      </a>
+    	  	</li>
+				<!-- TODO to be used -->
+	
+    		<!-- <ul class="navbar-nav ">
+    		  <li class="nav-item">
+    		    <a class="nav-link" href="#">
+    		      <i class="fa fa-bell">
+    		        <span class="badge badge-info">11</span>
+    		      </i>
+    		      Test
+    		    </a>
+    		  </li> -->
+    		</ul> 
+    		<form class="form-inline my-2 my-lg-0">
+    	  	<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+    	  	<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    		</form>
+  		</div>
+		</nav>
 	</div>
 </template>
 
 <style scoped>
-/* @import url("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"); */
+@import url("//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
+.logo-dimensions {
+	width: 110px;
+	height: 60px;
+}
 
 .navbar-icon-top .navbar-nav .nav-link > .fa {
   position: relative;
