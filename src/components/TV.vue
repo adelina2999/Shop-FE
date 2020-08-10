1d5c8a9de95c87a3f28380f301f074ea0cb86522
adelina2999 <tataru.adelina99@gmail.com>
2020-08-10 16:15:56 +0300

    cleared inputs on tv page

--- src/components/TV.vue
+++ src/components/TV.vue
@@ -1,23 +1,37 @@
 <template>
   <div>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Color</th>
-          <th scope="col">Price</th>
-        </tr>
-      </thead>
-      <tbody v-for="tv in tvs" :key="tv.id">
-        <tr :style="[tv.price <= 8000 ? { 'background-color': 'green' } : { 'background-color': 'red' }]" >
-          <th>{{ tv.id }}</th>
-          <td>{{ tv.name }}</td>
-          <td>{{ tv.color }}</td>
-          <td>{{ tv.price }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container">
+      <div class="form-group">
+        <input v-model="tvName" placeholder="Enter tv name" class="form-control" id="tvName">
+      </div>
+      <div class="form-group">
+        <input v-model="tvColor" placeholder="Enter tv color" class="form-control" id="tvColor">
+      </div>
+      <div class="form-group">
+        <input v-model="tvPrice" placeholder="Enter tv price" class="form-control" id="tvPrice">
+      </div>
+      <button @click="postTV()" class="btn btn-primary">Submit</button>
+    </div>
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Color</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody v-for="tv in tvs" :key="tv.id">
+          <tr :style="[tv.price <= 8000 ? { 'background-color': 'green' } : { 'background-color': 'red' }]" >
+            <th>{{ tv.id }}</th>
+            <td>{{ tv.name }}</td>
+            <td>{{ tv.color }}</td>
+            <td>{{ tv.price }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -28,7 +42,10 @@ export default {
   name: 'TVs',
   data () {
     return { 
-      tvs: []
+      tvs: [],
+      tvName: '',
+      tvColor: '',
+      tvPrice: null
     }
   },
   methods: {
@@ -36,6 +53,19 @@ export default {
       axios.get('http://localhost:5050/tv')
       .then(response => {
         this.tvs = response.data
+      })
+    },
+    postTV() {
+      axios.post('http://localhost:5050/tv', {
+        name: this.tvName,
+        color: this.tvColor,
+        price: this.tvPrice
+      })
+      .then(response => {
+        this.getTVs()
+        this.tvName = ''
+        this.tvColor = ''
+        this.tvPrice = null
       })
     }
   },
