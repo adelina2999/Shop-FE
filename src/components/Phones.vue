5becf4b8591c39c4f5950dd920207a84ccde7566
adelina2999 <tataru.adelina99@gmail.com>
2020-08-12 10:24:03 +0300

    added validation for phone page

--- src/components/Phones.vue
+++ src/components/Phones.vue
@@ -2,13 +2,29 @@
   <div>
     <div class="container">
       <div class="form-group">
-        <input v-model="phoneName" placeholder="Enter Phone name" class="form-control" id="phoneName">
+        <input v-model.trim="$v.phoneName.$model" :class="{ 'is-invalid':$v.phoneName.$error, 'is-valid':!$v.phoneName.$invalid }" placeholder="Enter Phone name" class="form-control" id="phoneName">
+        <div class="valid-feedback">Phone name is valid</div>
+        <div class="invalid-feedback">
+          <span v-if="!$v.phoneName.required">Phone name is required</span>
+          <span v-if="!$v.phoneName.minLength">Phone name must have at least {{ $v.phoneName.$params.minLength.min }} leters</span>
+          <span v-if="!$v.phoneName.maxLength">Phone name must have at most {{ $v.phoneName.$params.maxLength.max }} leters</span>
+        </div>
       </div>
       <div class="form-group">
-        <input v-model="phoneColor" placeholder="Enter Phone color" class="form-control" id="phoneColor">
+        <input v-model.trim="$v.phoneColor.$model" :class="{ 'is-invalid':$v.phoneColor.$error, 'is-valid':!$v.phoneColor.$invalid }" placeholder="Enter Phone color" class="form-control" id="phoneColor">
+        <div class="valid-feedback">Phone name is valid</div>
+        <div class="invalid-feedback">
+          <span v-if="!$v.phoneColor.required">Phone Color is required</span>
+          <span v-if="!$v.phoneColor.minLength">Phone Color must have at least {{ $v.phoneColor.$params.minLength.min }} leters</span>
+          <span v-if="!$v.phoneColor.maxLength">Phone Color must have at most {{ $v.phoneColor.$params.maxLength.max }} leters</span>
+        </div>
       </div>
       <div class="form-group">
-        <input v-model="phonePrice" placeholder="Enter Phone price" class="form-control" id="phonePrice">
+        <input type="number" v-model.number.lazy="$v.phonePrice.$model" :class="{ 'is-invalid':$v.phonePrice.$error, 'is-valid':!$v.phonePrice.$invalid }" placeholder="Enter Phone price" class="form-control" id="phonePrice">
+        <div class="valid-feedback">The price is valid</div>
+        <div class="invalid-feedback">
+          <span v-if="!$v.phonePrice.between">Must be between {{ $v.phonePrice.$params.between.min }} and {{ $v.phonePrice.$params.between.max }}</span>
+        </div>
       </div>
       <button @click="postPhone()" class="btn btn-primary">Submit</button>
     </div>
@@ -37,6 +53,7 @@
 
 <script>
 import axios from 'axios'
+import { required, minLength, maxLength, between } from 'vuelidate/lib/validators'
 
 export default {
   name: 'Phones',
@@ -46,6 +63,21 @@ export default {
       phoneName: '',
       phoneColor: '',
       phonePrice: null
+    }
+  },
+  validations: {
+    phoneName: {
+      required,
+      minLength: minLength(2),
+      maxLength: maxLength(15)
+    },
+    phoneColor: {
+      required,
+      minLength: minLength(3),
+      maxLength: maxLength(8)
+    },
+    phonePrice: {
+      between: between(50, 2000)
     }
   },
   methods: {
